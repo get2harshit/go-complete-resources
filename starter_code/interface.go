906fb03c9f02bb70eb3e4918315f68bd3d3d17ff
get2harshit <harshit.batra@polariscampus.com>
2026-02-26 07:21:39 +0530

    Define Database interface and outline implementation tasks
    Define Database interface and Mongo, MySQL structs. Outline tasks for implementing save methods and business logic.

--- starter_code/interface.go
+++ starter_code/interface.go
@@ -1,1 +1,29 @@
+package database
 
+import "fmt"
+
+// Interface (contract)
+type Database interface {
+	Save()
+}
+
+// Mongo implementation
+type Mongo struct{
+	Uri string
+}
+
+// Task 1: create save method for Mongo
+
+// MySQL implementation
+type MySQL struct{
+	Uri string
+}
+
+// Task 2: create save method for MySQL
+
+// Business logic depends on interface, not concrete DB
+func Process(db Database) {
+  // Task 3: write business logic
+}
+
+Complete all 3 tasks
